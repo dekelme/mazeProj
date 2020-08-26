899ac58da730ad0dab6ca48070ec334c449cbe6e
dekelme <61948586+dekelme@users.noreply.github.com>
2020-08-26 14:53:53 +0300

    Initial commit

---
+++ maze/main.cpp
@@ -0,0 +1,22 @@
+#include"cell.h"
+#include"maze.h"
+#include"state.h"
+#include"generator.h"
+#include"dirent.h"
+#include<istream>
+#include<ostream>
+#include<unordered_map>
+#include"zip.h"
+#include"bfs.h"
+#include"Astar.h"
+#include"controler.h"
+#include"Demo.h"
+
+
+
+int main()
+{
+	controler c(new CLI());
+	c.start(cin, cout);
+}
+
